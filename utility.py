c80a2ecddc369d376219d4948ffb98847e6b1d57
Shahrokh Bahtooei <42028078+ShahrokhBahtooei@users.noreply.github.com>
2021-03-15 19:18:58 +0330

    Initial commit

---
+++ utility.py
@@ -0,0 +1,21 @@
+from contextlib import contextmanager
+
+
+class Styles:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARK_CYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+
+@contextmanager
+def print_style(*styles):
+    print(*styles, sep='', end='')
+    yield
+    print(Styles.END, end='')
