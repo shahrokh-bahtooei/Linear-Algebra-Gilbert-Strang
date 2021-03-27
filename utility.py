023fb8288ad659bf1d07017fdf229976107bc374
Shahrokh Bahtooei <42028078+ShahrokhBahtooei@users.noreply.github.com>
2021-03-27 19:30:01 +0430

    Add graph for 3d discrete diff eqs
    Also fix 2d graphs to have the same scale for x and y axes

--- utility.py
+++ utility.py
@@ -1,4 +1,5 @@
 from contextlib import contextmanager
+import numpy as np
 
 
 class Styles:
@@ -19,3 +20,36 @@ def print_style(*styles):
     print(*styles, sep='', end='')
     yield
     print(Styles.END, end='')
+
+
+def draw_xyz_axes_at_center(mpl_ax):
+    # Compute max_lim based on plotted data
+    x_lim = abs(max(mpl_ax.get_xlim(), key=abs))
+    y_lim = abs(max(mpl_ax.get_ylim(), key=abs))
+    z_lim = abs(max(mpl_ax.get_zlim(), key=abs))
+    max_lim = max(x_lim, y_lim, z_lim)
+
+    # Position xyz axes to the center
+    mpl_ax.set_xlim(xmin=-max_lim, xmax=max_lim)
+    mpl_ax.set_ylim(ymin=-max_lim, ymax=max_lim)
+    mpl_ax.set_zlim(zmin=-max_lim, zmax=max_lim)
+
+    # Draw xyz axes
+    xyz_axes_appearance = dict(color='black', alpha=.5, lw=1, arrow_length_ratio=0.1)
+    labels = ['x', 'y', 'z']
+    for i in range(3):
+        starting_xyz = [0, 0, 0]
+        starting_xyz[i] = -max_lim
+
+        ending_xyz = [0, 0, 0]
+        ending_xyz[i] = max_lim
+
+        start_to_end_xyz = np.array(ending_xyz) - np.array(starting_xyz)
+        mpl_ax.quiver(*starting_xyz, *start_to_end_xyz, **xyz_axes_appearance)
+
+        ending_xyz_with_padding = np.array(ending_xyz) * 1.1
+        mpl_ax.text(*ending_xyz_with_padding,
+                    labels[i],
+                    horizontalalignment='center',
+                    verticalalignment='center',
+                    alpha=.5)
